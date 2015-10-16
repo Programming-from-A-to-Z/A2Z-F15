29cc12bab16905835dfd82a304b035b1ec16974e
Daniel Shiffman <daniel@shiffman.net>
2015-10-15 22:44:13 -0400

    add syllable counting to rita example #18

--- week5/03_rita_basics/sketch.js
+++ week5/03_rita_basics/sketch.js
@@ -1,3 +1,6 @@
+// A2Z F15
+// Daniel Shiffman
+// https://github.com/shiffman/A2Z-F15
 
 var sentenceInput, wordInput;
 
@@ -35,15 +38,41 @@ function analyze() {
   ol.parent(p);
 
   // What has the user entered?
+  // Make a rita string object
   var rs = new RiString(sentenceInput.value());
+  // Analyze that string for lots of features
   var features = rs.features();
 
+  // Here are some features you can get (there are more!)
   var li1 = createElement('li', 'Stresses: ' + features.stresses);
   var li2 = createElement('li', 'Phonemes: ' + features.phonemes);
   var li3 = createElement('li', 'Parts of speech: ' + features.pos);
+  var li4 = createElement('li', 'Syllables: ' + features.syllables);
+
+  // Put the in the list
   li1.parent(ol);
   li2.parent(ol);
   li3.parent(ol);
+  li4.parent(ol);
+
+  // How many syllables is each word?
+  
+  // First split up each word by anything not a dash, slash or letter/number
+  // This would be simpler if you are looking at one word at a time
+  var tokens = features.syllables.split(/[^\-\/\w]+/);
+  
+  // Make an array for the syllable count of each word
+  var syllableCount = [];
+
+  // How many syllables separated by slashes?
+  for (var i = 0; i < tokens.length-1; i++) {
+    var syllables = tokens[i].split(/\//);
+    syllableCount[i] = syllables.length;
+  }
+  
+  // Show this in the list
+  var li5 = createElement('li', 'Syllable count: ' + syllableCount.join(' '));
+  li5.parent(ol);
 }
 
 function rhyme() {
@@ -54,8 +83,10 @@ function rhyme() {
   var ol = createElement('ol');
   ol.parent(p);
 
+  // Get rhymes from the lexicon
   var rhymes = lex.rhymes(wordInput.value());
 
+  // Show them in a list
   for (var i = 0; i < rhymes.length; i++) {
     var li = createElement('li', rhymes[i]);
     li.parent(ol);
