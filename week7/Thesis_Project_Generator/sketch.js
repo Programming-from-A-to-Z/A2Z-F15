93331f5e8a702cbb6e19a2f80f808df537955153
Daniel Shiffman <daniel@shiffman.net>
2015-10-25 10:35:49 -0400

    thesis project generator

--- week7/Thesis_Project_Generator/sketch.js
+++ week7/Thesis_Project_Generator/sketch.js
@@ -6,22 +6,32 @@
 // 108-131
 // 4 8 31 44 48 60 73 81 89 101 107 96
 
+var venues = [4, 8, 31, 44, 48, 60, 73, 81, 89, 101, 107, 96];
+
 // An array of lines from a text file
 var lines;
 
 // The Markov Generator object
 var titleMarkov;
-var descriptionMarkov;
+var elevatorMarkov;
 
 function setup() {
   noCanvas();
 
   // The Markov Generator
   // First argument is N-gram length, second argument is max length of generated text
-  titleMarkov = new MarkovGenerator(3, 50);
-  descriptionMarkov = new MarkovGenerator(5, 2000);
+  titleMarkov = new MarkovGenerator(3, 40);
+  elevatorMarkov = new MarkovGenerator(6, 1000);
 
-  loadJSON('years/1981.json', process)
+  for (var i = 108; i < 132; i++) {
+    var url = 'https://itp.nyu.edu/projects/public/projectsJSON.php?venue_id=' + i;
+    loadJSON(url, process)
+  }
+
+  for (var i = 0; i < venues.length; i++) {
+    var url = 'https://itp.nyu.edu/projects/public/projectsJSON.php?venue_id=' + venues[i];
+    loadJSON(url, process)
+  }
 
   // Set up a button
   var button = select('#button');
@@ -29,9 +39,22 @@ function setup() {
 }
 
 function process(data) {
+  //console.log(data);
   for (var i = 0; i < data.length; i++) {
     titleMarkov.feed(data[i].project_name);
-    descriptionMarkov.feed(data[i].description);
+
+    var elevator = data[i].elevator_pitch;
+    if (elevator) {
+      // Doing some cleanup
+      elevator = elevator.replace(/&lt;/g,'<');
+      elevator = elevator.replace(/&gt;/g,'>');
+
+
+      elevator = elevator.replace(/&.*?;/g,'');
+      elevator = elevator.replace(/<.*?>/g,'');
+
+      elevatorMarkov.feed(elevator);
+    }
   }
 }
 
@@ -43,6 +66,6 @@ function generate() {
   title.html(titleMarkov.generate());
 
   var description = select('#description');
-  description.html(descriptionMarkov.generate());
+  description.html(elevatorMarkov.generate());
 
 }
