dede44b04ed1d42ef8c48506b6eff997216a6d23
Daniel Shiffman <daniel@shiffman.net>
2015-10-27 22:00:50 -0400

    more comments and cleanup

--- week7/10_many_elements_test/sketch.js
+++ week7/10_many_elements_test/sketch.js
@@ -1,23 +1,35 @@
+// A2Z F15
+// Daniel Shiffman
+// https://github.com/shiffman/A2Z-F15
 
 function setup() {
+
+  // Make a tiny canvas for loading animation
   var canvas = createCanvas(50, 50);
   canvas.parent('canvas');
 
+  // Make 5,000 DOM elements one a time
+  // Using a function that triggers the action with a delay
   for (var i = 0; i < 5000; i++) {
     makeIt(i);
   }
 
-  function makeIt(i) {
-    setTimeout(reallyMakeIt, i*5);
+  function makeIt(count) {
+    // Do this every 5 milliseconds
+    setTimeout(reallyMakeIt, count*5);
+
+    // This "closure" gives us access to the value of count
+    // which was filled from i in the original loop
     function reallyMakeIt() {
-      var span = createSpan(i + ' ');
+      var span = createSpan(count + ' ');
       span.parent('stuff');
     }
   }
 }
 
-var angle = 0;
 
+var angle = 0;
+// Just a silly loading animation
 function draw() {
   background(200);
   translate(width/2, height/2);
