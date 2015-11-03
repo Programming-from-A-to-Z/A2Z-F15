d167387c64c997f97633b78d90339cf4916f5f8b
Daniel Shiffman <daniel@shiffman.net>
2015-11-03 10:25:49 -0500

    add comments to all the closure examples

--- week8/12_closure_for_animation_part2/sketch.js
+++ week8/12_closure_for_animation_part2/sketch.js
@@ -5,29 +5,44 @@
 function setup() {
   noCanvas();
 
+  // Now make 500 of animating elements
+  // Every 10 milliseconds
   for (var i = 0; i < 500; i++) {
     setTimeout(makeElement, i*10);
   }
 }
 
+// A functon for making the element
 function makeElement() {
-  var span = createSpan('nothing');
+
+  // Make the element and animate it!
+  var span = createSpan('0');
+  animate(span);
+
+  // This is silly but putting a space between them for spacing
   var space = createSpan(' ');
+  // Just some styling
   space.parent('padded');
   span.class('box');
   span.parent('padded');
-  animate(span);
 }
 
 
+// A closure for animating
 function animate(elt) {
-
+  // count and elt are local to this function but live on since
+  // increment continues to be called via setInterval
   var count = 0;
+
+  // Just update the contents of the DOM element
   function increment() {
     count = (count + 1) % 100;
     elt.html(count);
   }
-  setInterval(increment, 25);
+
+  // Exercuse increment every 100 milliseconds
+  setInterval(increment, 100);
 }
+
 
 
