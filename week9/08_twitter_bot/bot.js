a5ab00bbe68ba608a809a1a0b3aa80433496c0c1
Daniel Shiffman <daniel@shiffman.net>
2015-11-09 14:00:46 -0500

    whoops API key

--- week9/08_twitter_bot/bot.js
+++ week9/08_twitter_bot/bot.js
@@ -2,13 +2,10 @@
 // npm install twit
 var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         'bBp7yRjjICxeNsknpwvms4lSa', 
-  consumer_secret:      'aQ4hry8nS0zh2puj2PSLsNljmK3sRqdRRnn8NxFF0p7rSXbLx9',
-  access_token:         '2799041194-VQaiuOUCk6Z8Wwk0ZMDUwtg1njc4WCzRkb2MueC',
-  access_token_secret:  'APGaYOHs9RDkThiizzWPpC1aGYsj123R4w1to9AqcivTL'
-});
-
+// Pulling all my twitter account info from another file
+var config = require('./config.js');
+// Making a Twit object for connection to the API
+var T = new Twit(config);
 
 var count = 35;
 
